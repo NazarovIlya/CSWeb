a6d5b47eecb351d12c0660a01eaa5e9208290ebb
NazarovIlya <eliakohai@gmail.com>
2022-08-20 20:37:41 +0300

    route mthods added

--- CSW_0000/Program.cs
+++ CSW_0000/Program.cs
@@ -4,3 +4,9 @@ var app = builder.Build();
 app.MapGet("/", () => "Hello C# Web!");
 
 app.Run();
+
+app.MapGet("/about", () => RouteMethods.About());
+
+app.MapGet("/hi/{name}", (string name) => RouteMethods.Hi(name));
+
+app.MapGet("/calc/{x},{y}", (int x, int y) => RouteMethods.Sum(x, y));
