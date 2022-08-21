95ad886693dd2a05f5f83a102780232af01e2233
NazarovIlya <eliakohai@gmail.com>
2022-08-21 05:58:05 +0300

    my first web cs app

--- CSW_0000/Program.cs
+++ CSW_0000/Program.cs
@@ -3,10 +3,10 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello C# Web!");
 
-app.Run();
-
 app.MapGet("/about", () => RouteMethods.About());
 
 app.MapGet("/hi/{name}", (string name) => RouteMethods.Hi(name));
 
 app.MapGet("/calc/{x},{y}", (int x, int y) => RouteMethods.Sum(x, y));
+
+app.Run();
