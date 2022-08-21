35842573117dbf31236fc30a838b08b9caf7aef3
NazarovIlya <eliakohai@gmail.com>
2022-08-21 14:05:44 +0300

    contoller class added

--- CSW_0000/Controller/StudentController.cs
+++ CSW_0000/Controller/StudentController.cs
@@ -1,7 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
 public class StudentController : Controller
 {
-    public JsonResult Get()
+    // public JsonResult Get()
+    // {
+    //   return Json(Repository.GetStudents());
+    // }
+
+    public JsonResult Get(int arg = 5)
     {
-        return Json(Repository.GetStudents());
+        return Json(Repository.GetStudents(arg));
     }
 }
